1fb1a09035244b63f97fcd1cd7d25e62a60cc1a2
John Nguyen <johnn.mail@icloud.com>
2020-03-12 00:40:37 -0500

    changes

--- shutdown-es.sh
+++ shutdown-es.sh
@@ -1,3 +1,5 @@
 kubectl delete pod elasticsearch-0
 kubectl delete statefulset elasticsearch
 kubectl delete services elasticsearch
+kubectl delete deployment kibana-deployment
+kubectl delete services kibana
